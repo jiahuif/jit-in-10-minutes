25d0e4ef406f2516174b77b1c94b49ff01623fc7
Indeed <indeed@indeed.moe>
2016-12-20 18:36:50 +0800

    polish

--- p1s.c
+++ p1s.c
@@ -18,18 +18,21 @@ int64_t incr(int64_t x)
 int main(int argc, char ** argv)
 {
     struct code_block * block = create_code_block();
-	if (argc < 2) {
-		fprintf(stderr, "Usage: %s INFILE\n", argv[0]);
-		return EXIT_FAILURE;
-	}
-	FILE * text_source = fopen(argv[1], "r");
-	if (!text_source) {
-		perror("fopen");
-		return EXIT_FAILURE;
-	}
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s INFILE\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    FILE * text_source = fopen(argv[1], "r");
+    if (!text_source)
+    {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
     int64_t * byte_code = compile_text_file(text_source);
-	if (!byte_code)
-		return EXIT_FAILURE;
+    fclose(text_source);
+    if (!byte_code)
+        return EXIT_FAILURE;
     tier1_generate_function(block, byte_code);
     GeneratedFunction f = (GeneratedFunction) make_block_executable(block);
     printf("%" PRId64 " " "%" PRId64 "\n", f(0), tier0_interpret(byte_code));
